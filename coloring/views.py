89ec10815f9ceab4eae21140d7fda7eba6a30ded
Anze Liu <anzeliu0604@berkeley.edu>
2021-07-14 15:42:59 -0700

    finished project3

--- coloring/views.py
+++ coloring/views.py
@@ -2,3 +2,21 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'coloring/index.html')
+
+def demo(request):
+    return render(request, 'coloring/demo.html')
+
+def new_interaction(request):
+    return render(request, 'coloring/new_interaction.html')
+
+def home(request):
+    return render(request, 'coloring/home.html')
+
+def drawPad(request):
+    return render(request, 'coloring/drawPad.html')
+
+def gallery(request):
+    return render(request, 'coloring/gallery.html')
+
+def colorPicker(request):
+    return render(request, 'coloring/colorPicker.html')
